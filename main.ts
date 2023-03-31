9754454f48db9e7f12e77660fbe8b064f4fdc953
4hiziri <meirvg@gmail.com>
2023-03-31 19:13:59 +0900

    Add basic move command

--- main.ts
+++ main.ts
@@ -1,13 +1,69 @@
 import { getCurves } from 'crypto';
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Scope, Hotkey, Modal, Notice, Plugin, PluginSettingTab, Setting, EditorPosition } from 'obsidian';
+
+// editorCheckCallback: デフォルトのホットキーと衝突させないためのCallback
+// デフォルトで衝突してしまうものだからどうしようか
+// 衝突するキーマップを解除した.obsidian/hotkeys.jsonを直接配布?
+// C-M-a - editor.exec('goStart'); // これはこれで使えるのでメモしておく
 
 export default class EmacsHotkey extends Plugin {
 	async onload() {
 		this.addCommand({
-			id: 'emacs-goto-head',
-			name: 'Emacs go to head',
+			id: 'emacs-head-line',
+			name: 'Go head line',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'a' }],
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				new Notice(editor.getCursor().toString());
+				let pos: EditorPosition = editor.getCursor();
+				pos.ch = 0;
+				editor.setCursor(pos);
+			}
+		});
+
+		this.addCommand({
+			id: 'emacs-line-end',
+			name: 'Go end line',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'e' }],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				let pos: EditorPosition = editor.getCursor();
+				let len = editor.getLine(pos.line).length;
+				pos.ch = len;
+				editor.setCursor(pos);
+			}
+		});
+
+		this.addCommand({
+			id: 'emacs-previous-line',
+			name: 'Go previous line',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'p' }],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.exec('goUp');
+			}
+		});
+
+		this.addCommand({
+			id: 'emacs-next-line',
+			name: 'Go next line',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'n' }],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.exec('goDown');
+			}
+		});
+
+		this.addCommand({
+			id: 'emacs-forward-char',
+			name: 'Go forward char',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'f' }],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.exec('goRight');
+			}
+		});
+
+		this.addCommand({
+			id: 'emacs-backward-char',
+			name: 'Go backward char',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'b' }],
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.exec('goLeft');
 			}
 		});
 	}
