49f346670b6ed372f051fe57723ac2fbda4da66b
4hiziri <meirvg@gmail.com>
2023-03-31 22:28:13 +0900

    Add kill yank, copy from inouetakuya/obsidian-kill-and-yank

--- main.ts
+++ main.ts
@@ -6,7 +6,19 @@ import { App, Editor, MarkdownView, Scope, Hotkey, Modal, Notice, Plugin, Plugin
 // 衝突するキーマップを解除した.obsidian/hotkeys.jsonを直接配布?
 // C-M-a - editor.exec('goStart'); // これはこれで使えるのでメモしておく
 
+// https://github.com/inouetakuya/obsidian-kill-and-yank/
+// からkill yankを実装
+// MITだから適当にREADMEの最後にでも記載する
+// TODO: ライセンスとかに色々反映
+
 export default class EmacsHotkey extends Plugin {
+	private mark: EditorPosition | null = null;
+
+	// private isComposing(view: MarkdownView): boolean {
+	// 	const editorView = view.editor.cm as EditorView;
+	// 	return editorView.composing;
+	// }
+
 	async onload() {
 		this.addCommand({
 			id: 'emacs-head-line',
@@ -25,7 +37,7 @@ export default class EmacsHotkey extends Plugin {
 			hotkeys: [{ modifiers: ['Ctrl'], key: 'e' }],
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				let pos: EditorPosition = editor.getCursor();
-				let len = editor.getLine(pos.line).length;
+				const len = editor.getLine(pos.line).length;
 				pos.ch = len;
 				editor.setCursor(pos);
 			}
@@ -66,9 +78,75 @@ export default class EmacsHotkey extends Plugin {
 				editor.exec('goLeft');
 			}
 		});
+
+		this.addCommand({
+			id: 'emacs-kill-line',
+			name: 'Emacs kill line',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'k' }],
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				// if (this.isComposing(view)) return;
+				new Notice(checking.toString());
+
+				const pos: EditorPosition = editor.getCursor();
+				const line: string = editor.getLine(pos.line);
+
+				const textToBeRetained = line.slice(0, pos.ch);
+				const textToBeCut = line.slice(pos.ch);
+
+				navigator.clipboard.writeText(textToBeCut);
+
+				editor.setLine(pos.line, textToBeRetained);
+				// editor.setCursor(pos, pos.ch);
+			},
+		})
+
+		this.addCommand({
+			id: 'emacs-kill-region',
+			name: 'Emacs kill region',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'w' }],
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				// if (this.isComposing(view)) return
+				new Notice(checking.toString());
+
+				if (this.mark) {
+					editor.setSelection(this.mark, editor.getCursor());
+					this.mark = null;
+				}
+				navigator.clipboard.writeText(editor.getSelection());
+				editor.replaceSelection('');
+			},
+		})
+
+		this.addCommand({
+			id: 'emacs yank',
+			name: 'Emacs yank',
+			hotkeys: [{ modifiers: ['Ctrl'], key: 'y' }],
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				// if (this.isComposing(view)) return
+				new Notice(checking.toString());
+
+				navigator.clipboard.readText().then((text) => {
+					editor.replaceSelection(text);
+				})
+			},
+		})
+
+		this.addCommand({
+			id: 'set-mark',
+			name: 'Set mark (Toggle the start position of the selection)',
+			hotkeys: [{ modifiers: ['Ctrl'], key: ' ' }],
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				new Notice(checking.toString());
+
+				if (this.mark) {
+					editor.setSelection(this.mark, editor.getCursor());
+					this.mark = null;
+				} else {
+					this.mark = editor.getCursor();
+				}
+			},
+		})
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 }
